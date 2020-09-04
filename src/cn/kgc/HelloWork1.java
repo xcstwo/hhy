04e4811c8ed358dfcf30e693207df5c9562de074
xcstwo <3058963049@qq.com>
2020-09-04 19:00:36 +0800

    11111

---
+++ src/cn/kgc/HelloWork1.java
@@ -0,0 +1,13 @@
+package cn.kgc;
+
+public class HelloWork1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("11111");
+	}
+
+}

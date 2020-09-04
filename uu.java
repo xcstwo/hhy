09f5a5153a83a5ceec4c760c414655503de306fa
xcstwo <70744829+xcstwo@users.noreply.github.com>
2020-09-04 17:45:44 +0800

    Add files via upload

---
+++ uu.java
@@ -0,0 +1,13 @@
+package cn.bdqn;
+
+public class uu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("jj");
+	}
+
+}
